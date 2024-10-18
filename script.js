e26e4b157dabf09b9d4b18d26786c29e10fcaae5
tonyloba <tonyloba@gmail.com>
2024-10-17 20:40:24 -0400

    updated names and function form

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ btn.addEventListener("click", function () {
   console.log("Clicked");
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
+    btn.textContent = "Close";
   } else {
     form.classList.add("hidden");
+    btn.textContent = "Set a trip";
   }
 });
