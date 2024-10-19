989ad3dd0908e0b9d2b9a96be903ef4b29271f40
tonyloba <tonyloba@gmail.com>
2024-10-19 19:32:34 -0400

    added func asunc to get API data from SupaBase

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-// console.log("Helllllos");
-
+// Selecting DOM Elements:
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
 const factsList = document.querySelector(".facts-list");
 
-factsList.innerHTML = "";
+//Creat DOM Elements and render post in the list
+//factsList.innerHTML = "";
 
+loadPosts();
+
+async function loadPosts() {
+  //Load DATA from Supabase:
+  const res = await fetch(
+    "https://nzhzhxhydfeyqnqvicci.supabase.co/rest/v1/Trips",
+    {
+      //mode: "no-cors",
+      headers: {
+        apikeys:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56aHpoeGh5ZGZleXFucXZpY2NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkwMzMwMTksImV4cCI6MjA0NDYwOTAxOX0.grwBrpB8DAKTOajH4zhneBiiOatWFix6yOVgksNfyz4",
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56aHpoeGh5ZGZleXFucXZpY2NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkwMzMwMTksImV4cCI6MjA0NDYwOTAxOX0.grwBrpB8DAKTOajH4zhneBiiOatWFix6yOVgksNfyz4",
+      },
+    }
+  );
+  const data = await res.json();
+  // here shoud be whih load from DB to DOM html:  createFactsList(data)
+}
+
+// Switch form visibility:
 btn.addEventListener("click", function () {
   console.log("Clicked");
   if (form.classList.contains("hidden")) {
