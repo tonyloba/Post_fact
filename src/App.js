b5c53ab8ad2556e3831bdae542f9e7d77bf5be6a
tonyloba <tonyloba@gmail.com>
2024-10-28 00:07:42 -0400

    added some jsx for react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import "./style.css";
+
 function App() {
-  return <h1>Helloo!</h1>
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" height="68" width="68" alt="Facts Logo" />
+        <h1>Post a Trip</h1>
+      </div>
+
+      <button className="btn btn-large btn-open">Set a trip</button>
+    </header>
+  );
 }
 
 export default App;
